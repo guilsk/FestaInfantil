160ea4e140f993bca7f7ed17c786669b85b4cbd8
guilsk <guilherme.gmdp@gmail.com>
2023-06-09 15:17:34 -0300

    First Commit

---
+++ FestaInfantil/Compartilhado/EntidadeBase.cs
@@ -0,0 +1,12 @@
+﻿namespace FestaInfantil.Compartilhado
+{
+    [Serializable]
+    public abstract class EntidadeBase<TEntidade>
+    {
+        public int id;
+
+        public abstract void AtualizarInformacoes(TEntidade registroAtualizado);
+
+        public abstract string[] Validar();
+    }
+}
